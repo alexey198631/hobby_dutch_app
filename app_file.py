d3548af82d8a6aea92604609b7c190ca33c45411
Alex <al.an.gukov@gmail.com>
2023-01-07 22:23:22 +0100

    project start commit

---
+++ app_file.py
@@ -0,0 +1,6 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog
+from PyQt6.QtGui import QPixmap
+from PyQt6 import QtGui, QtCore
+from PyQt6.QtGui import QCursor
+
